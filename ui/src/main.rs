a3c1d7001247955bd8cabddab6b2daaafb53b092
sak96 <26397224+sak96@users.noreply.github.com>
2022-06-23 23:02:26 +0530

    Center other stuff

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,10 +1,11 @@
 use crate::utils::weak_component_link::WeakComponentLink;
 use components::timer::{Timer, TimerMsg};
+use stylist::css;
 use yew::prelude::*;
 
 pub mod components;
-pub mod utils;
 pub mod tauri;
+pub mod utils;
 
 struct App {}
 
@@ -22,29 +23,54 @@ impl Component for App {
         let reset_timer = {
             let timer_link = timer_link.clone();
             move |_| {
-                timer_link.borrow_clone_unwrap().send_message(TimerMsg::ResetTimer(100))
+                timer_link
+                    .borrow_clone_unwrap()
+                    .send_message(TimerMsg::ResetTimer(100))
             }
         };
         let pause_timer = {
             let timer_link = timer_link.clone();
             move |_| {
-                timer_link.borrow_clone_unwrap().send_message(TimerMsg::PauseTimer)
+                timer_link
+                    .borrow_clone_unwrap()
+                    .send_message(TimerMsg::PauseTimer)
             }
         };
         let resume_timer = {
             let timer_link = timer_link.clone();
             move |_| {
-                timer_link.borrow_clone_unwrap().send_message(TimerMsg::ResumeTimer)
+                timer_link
+                    .borrow_clone_unwrap()
+                    .send_message(TimerMsg::ResumeTimer)
             }
         };
+
         html! {
-            <>
-                <h1>{ "Hello World" }</h1>
+            <div
+                class={css!(r#"
+                    display: flex;
+                    flex-wrap: wrap;
+                    align-items: center;
+                    flex-direction: column;
+                    justify-content: center;
+                "#)}
+            >
+                <h1>{ "Pomodoro Timer" }</h1>
                 <Timer weak_link={timer_link}></Timer>
-                <button onclick={reset_timer}>{ "Reset" }</button>
-                <button onclick={pause_timer}>{ "Pause" }</button>
-                <button onclick={resume_timer}>{ "Resume" }</button>
-            </>
+                <div
+                class={css!(r#"
+                    display: flex;
+                    flex-wrap: wrap;
+                    button {
+                        margin: 10px;
+                    }
+                "#)}
+                >
+                    <button onclick={reset_timer}>{ "Reset" }</button>
+                    <button onclick={pause_timer}>{ "Pause" }</button>
+                    <button onclick={resume_timer}>{ "Resume" }</button>
+                </div>
+            </div>
         }
     }
 }
